bb4b649c5e9bcf14fca782f06254a88b54c98c7a
shanil2255 <85777175+shanil2255@users.noreply.github.com>
2021-06-20 09:54:37 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,21 +4,21 @@
 
 class Translation(object):
     
-    START_TEXT = """<b>Hey {}!!</b>
+    START_TEXT = """<b><u>Hey {} Hpw Are You!!</b></u>
 <i>Am Just A Advance Auto Filter Bot....😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+For Movies, 18+ Troll's, Suggestions, Trolls, Movie Updates,, Follow Our Channel @trollcinemates 🔥
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@trollcinemates
 </i>"""    
     
     HELP_TEXT = """
 <b><i><u>How To Use Me!?</u></i></b>
 
 <i>
--> Add Me To Any Group And Make Me Admin
--> Add Me To Your Desired Channel
+-> Iam The Bot Of Troll Cinemates 
+-> Dont Add Me To Any Group.. I Will Kill You
 </i>
 
 <b>Bot Commands (Works Only In Groups) :</b>
@@ -43,16 +43,16 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@trollcinemates
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/null_fallaci">shhaanil</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/shanil2255/troll">Click Me</a></i>
 """
